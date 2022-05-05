cefc26e8a5897684c72eb8485ac321fec81ef3b8
Lucas Andrade <lucasantos.dev@gmail.com>
2022-05-05 14:32:49 -0300

    update routes

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,46 +1,10 @@
 import express from 'express'
-import nodemailer from 'nodemailer'
-import { prisma } from './prisma'
+import { routes } from './routes'
 
 const app = express()
 
 app.use(express.json())
-
-const transport = nodemailer.createTransport({
-  host: "smtp.mailtrap.io",
-  port: 2525,
-  auth: {
-    user: "f6d2d9ddde026d",
-    pass: "34524005301bb8"
-  }
-});
-
-app.post('/feedbacks', async (req, res) => {
-  const { type, comment, screenshot } = req.body
-
-  const feedback = await prisma.feedback.create({
-    data: {
-      type,
-      comment,
-      screenshot
-    }
-  })
-
-  await transport.sendMail({
-    from: 'Equipe Feedget <hello@feedget.com>',
-    to: 'Lucas Andrade <lucasantos.dev@gmail.com>',
-    subject: 'Novo feedback',
-    html: [
-      `<div style="font-family: sans-serif; font-size: 16px; color: #111">`,
-      `<p>Tipo do feedback: ${type}</p>`,
-      `<p>Comentário: ${comment}</p>`,
-      `</div>`
-    ].join('\n')
-  })
-
-  res.status(201)
-  return res.json({ data: feedback })
-})
+app.use(routes)
 
 app.listen(3333, () => {
   console.log('Server is running...')
