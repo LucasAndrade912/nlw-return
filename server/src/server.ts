9324d1eb134443112615dbca89d4a23c90459801
Lucas Andrade <lucasantos.dev@gmail.com>
2022-05-06 23:43:43 -0300

    configure port of the backend

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,11 +3,12 @@ import cors from 'cors'
 import { routes } from './routes'
 
 const app = express()
+const port = process.env.PORT || 3333
 
 app.use(cors())
 app.use(express.json())
 app.use(routes)
 
-app.listen(3333, () => {
+app.listen(port, () => {
   console.log('Server is running...')
 })
