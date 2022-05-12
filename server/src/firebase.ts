f719456961c82046b6155e61d471e7f5843afe5c
Lucas Andrade <lucasantos.dev@gmail.com>
2022-05-12 19:21:12 -0300

    update firebase config

--- server/src/firebase.ts
+++ server/src/firebase.ts
@@ -1,7 +1,14 @@
 import { initializeApp, cert } from 'firebase-admin/app'
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 export function initFirebaseApp() {
-	const serviceAccount = require('../serviceAccountKey.json')
+	const serviceAccount = {
+		projectId: process.env.PROJECT_ID,
+		clientEmail: process.env.CLIENT_EMAIL,
+		privateKey: process.env.PRIVATE_KEY
+	}
 
 	initializeApp({
 		credential: cert(serviceAccount)
