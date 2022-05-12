2c3e536e795910ffd116f890d4d5116dc85357d4
Lucas Andrade <lucasantos.dev@gmail.com>
2022-05-12 15:18:42 -0300

    update tests

--- server/src/useCases/SubmitFeedbackUseCase.test.ts
+++ server/src/useCases/SubmitFeedbackUseCase.test.ts
@@ -1,16 +1,20 @@
 import { SubmitFeedbackUseCase } from "./SubmitFeedbackUseCase"
 
 const createFeedbackSpy = jest.fn()
+const findAllFeedbacksSpy = jest.fn()
 const sendMailSpy = jest.fn()
 
+const userIdForTest = '7f92ed6f-890a-47be-87d6-ac22d769e2be'
+
 const submitFeedbackUseCase = new SubmitFeedbackUseCase(
-  { create: createFeedbackSpy },
+  { create: createFeedbackSpy, findAll: findAllFeedbacksSpy },
   { sendMail: sendMailSpy }
 )
 
 describe('Submit feedback', () => {
   it('should be able to submit a feedback', async () => {
     await expect(submitFeedbackUseCase.execute({
+      userId: userIdForTest,
       type: 'BUG',
       comment: 'Comment example',
       screenshot: 'data:image/png;base64,2932932392j92je'
@@ -22,6 +26,7 @@ describe('Submit feedback', () => {
 
   it('should not be able to submit a feedback without type', async () => {
     await expect(submitFeedbackUseCase.execute({
+      userId: userIdForTest,
       type: '',
       comment: 'Comment example',
       screenshot: 'data:image/png;base64,2932932392j92je'
@@ -30,6 +35,7 @@ describe('Submit feedback', () => {
 
   it('should not be able to submit a feedback without comment', async () => {
     await expect(submitFeedbackUseCase.execute({
+      userId: userIdForTest,
       type: 'BUG',
       comment: '',
       screenshot: 'data:image/png;base64,2932932392j92je'
@@ -38,6 +44,7 @@ describe('Submit feedback', () => {
 
   it('should not be able to submit a feedback with an invalid screenshot', async () => {
     await expect(submitFeedbackUseCase.execute({
+      userId: userIdForTest,
       type: 'BUG',
       comment: 'Comment example',
       screenshot: 'test.png'
