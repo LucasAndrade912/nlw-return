341d71a8177f90927fc9140da5b54c61cb44d321
Lucas Andrade <lucasantos.dev@gmail.com>
2022-05-12 14:50:44 -0300

    update database tables

---
+++ server/prisma/migrations/20220511221412_create_users/migration.sql
@@ -0,0 +1,17 @@
+-- AlterTable
+ALTER TABLE "feedbacks" ADD COLUMN     "user_id" TEXT;
+
+-- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
+
+-- AddForeignKey
+ALTER TABLE "feedbacks" ADD CONSTRAINT "feedbacks_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE SET NULL ON UPDATE CASCADE;
