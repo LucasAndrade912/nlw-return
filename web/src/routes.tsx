5300a391464d12be2efc43be7dd73ae99aa25d02
Lucas Andrade <lucasantos.dev@gmail.com>
2022-05-14 22:54:03 -0300

    create state for authentication

--- web/src/routes.tsx
+++ web/src/routes.tsx
@@ -1,18 +1,31 @@
-import React from 'react'
+import React, { createContext, SetStateAction, useState } from 'react'
 import { Routes, Route } from 'react-router-dom'
 import { App } from './App'
 import { Dashboard } from './pages/Dashboard'
 import { Home } from './pages/Home'
 import { Login } from './pages/Login'
 
+type AuthContextProps = [
+  authenticated: boolean | null,
+  setAuthenticated: React.Dispatch<SetStateAction<boolean | null>>
+]
+
+export const AuthContext = createContext<AuthContextProps | null>(null)
+
 export default () => {
+  const userToken = localStorage.getItem('token')
+
+  const [authenticated, setAuthenticated] = useState<boolean | null>(userToken ? true : false)
+  
   return (
-    <Routes>
-      <Route path="/" element={<App />}>
-        <Route path="home" element={<Home />} />
-        <Route path="dashboard" element={<Dashboard />} />
-      </Route>
-      <Route path="login" element={<Login />} />
-    </Routes>
+    <AuthContext.Provider value={[ authenticated, setAuthenticated ]}>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route path="home" element={<Home />} />
+          <Route path="dashboard" element={<Dashboard />} />
+        </Route>
+        <Route path="login" element={<Login />} />
+      </Routes>
+    </AuthContext.Provider>
   )
 }
